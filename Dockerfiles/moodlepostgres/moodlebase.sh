0199c8c796ae228ae3bf9fca064957038c965b07
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-03-13 14:56:50 +0100

    Add Dockerfiles for nginx, php-fpm, postgres

---
+++ Dockerfiles/moodlepostgres/moodlebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Create `$MOODLE_DBUSER' role and corresponding db for use by the Moodle installation.
+set -e
+createuser --username "$POSTGRES_USER" "$MOODLE_DBUSER"
+createdb --username "$POSTGRES_USER" -O "$MOODLE_DBUSER" "$MOODLE_DBUSER"
+gosu postgres psql -c "ALTER USER $MOODLE_DBUSER WITH PASSWORD '$MOODLE_DBPASS';"
